cb9b5bbf2c54f2818bd00561b9e7ab963bf9d3d2
vijay9711 <vijaysm97@gmail.com>
2024-02-05 11:56:37 -0600

    context setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RoleProvider } from './context/roleContext';
+import { Home } from './container/Home/Home';
 function App() {
   return (
-    <div className="App">
-    <h1 className='text-xl text-blue-400'>
-    heelo
-    </h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
+    <div>
+      <RoleProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </RoleProvider>
     </div>
   );
 }
---
+++ src/context/roleContext.js
@@ -0,0 +1,27 @@
+import React, { useCallback, useEffect, useReducer, useState, createContext, useContext } from "react";
+
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'update':
+      return { id: action.payload.id, name: action.payload.name, role: action.payload.role };
+    default:
+      return state;
+  }
+}
+const RoleContext = createContext();
+
+export const RoleProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, { id: null, name:'', role:'' });
+
+  return (
+    <RoleContext.Provider value={{ state, dispatch }}>
+      {children}
+    </RoleContext.Provider>
+  )
+}
+
+export const useRoleContext = () => {
+  return useContext(RoleContext);
+}
